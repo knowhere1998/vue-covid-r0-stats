3b5df183a71e03633339af0266617ab48253ed61
knowhere1998 <darekar.amey@gmail.com>
2020-06-17 01:19:40 +0530

    Defined initial project structure

---
+++ src/components/Nav.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="nav">
+		<nav>
+			<h1>R-Nought</h1>
+		</nav>
+	</div>
+</template>
+
+<script>
+
+export default{
+	name: 'Nav',
+}
+</script>
+
+<style scoped>
+	
+</style>

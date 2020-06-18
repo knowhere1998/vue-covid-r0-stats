8c98b9e2504e308bff7e2714b1cdf154e9636d31
knowhere1998 <darekar.amey@gmail.com>
2020-06-18 13:49:34 +0530

    Indentation fix

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,33 @@
 <template>
-  <div id="app">
-    <Nav/>
-    <Content/>
-  </div>
+	<div id="app">
+		<Nav />
+		<Content class="flex-grow" />
+		<Footer />
+	</div>
 </template>
 
 <script>
 import Nav from './components/Nav.vue'
 import Content from './components/Content.vue'
+import Footer from './components/Footer.vue'
 
 export default {
-  name: 'App',
-  components: {
-    Nav,
-    Content
-  }
-}
+	name: 'App',
+	components: {
+		Nav,
+		Content,
+		Footer
+	}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	display: flex;
+	min-height: 100vh;
+	flex-direction: column;
 }
 </style>

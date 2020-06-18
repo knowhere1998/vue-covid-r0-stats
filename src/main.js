380f38afea6cab45af256b93436164d8d50b0f89
knowhere1998 <darekar.amey@gmail.com>
2020-06-18 17:39:28 +0530

    switching to apexcharts from chartjs

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import '@/assets/css/tailwind.css'
-import "vue-select/dist/vue-select.css"
-import vSelect from "vue-select"
+import Vue from 'vue';
+import App from './App.vue';
+import '@/assets/css/tailwind.css';
+import 'vue-select/dist/vue-select.css';
+import vSelect from 'vue-select';
+import VueApexCharts from 'vue-apexcharts';
+import moment from 'moment';
 
+Vue.component('apexchart', VueApexCharts)
 Vue.component("v-select", vSelect);
+Vue.prototype.moment = moment
 Vue.config.productionTip = false
 
 new Vue({

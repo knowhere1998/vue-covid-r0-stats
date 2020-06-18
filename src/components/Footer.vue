8611e28736db0bfaa753399e85948bb6710d2370
knowhere1998 <darekar.amey@gmail.com>
2020-06-18 13:49:56 +0530

    Added footer Component

---
+++ src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+
+  <section class="footer">
+    <h1>footer Component</h1>
+  </section>
+
+</template>
+
+<script>
+
+  export default  {
+    name: 'Footer',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped>
+  .footer {
+
+  }
+</style>

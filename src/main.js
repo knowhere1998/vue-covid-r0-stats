e38e21856f146f60201737d6339db39dd9688cd4
knowhere1998 <darekar.amey@gmail.com>
2020-06-19 15:30:28 +0530

    Added tailwind-toggle plugin

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import '@/assets/css/tailwind.css';
 import 'vue-select/dist/vue-select.css';
 import vSelect from 'vue-select';
 import VueApexCharts from 'vue-apexcharts';
+import AxiosPlugin from 'vue-axios-cors';
 import moment from 'moment';
-
+import { ToggleButton } from 'vue-js-toggle-button'
+ 
+Vue.component('ToggleButton', ToggleButton)
 Vue.component('apexchart', VueApexCharts)
 Vue.component("v-select", vSelect);
 Vue.prototype.moment = moment
 Vue.config.productionTip = false
+Vue.use(AxiosPlugin);
 
 new Vue({
   render: h => h(App),

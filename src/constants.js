85c45a7cc930b9372fb034202e1c80db1dbf592c
knowhere1998 <darekar.amey@gmail.com>
2020-06-26 20:46:24 +0530

    Added a constants collection

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export default {
+	'API_URL': "https://node-covid-data.herokuapp.com",
+	'questions' : [
+		{"question": "1+1", "answer": "2", "active": true},
+		{"question": "1+2", "answer": "3", "active": false},
+		{"question": "1+3", "answer": "4", "active": false},
+		{"question": "1+4", "answer": "5", "active": true},
+		{"question": "1+5", "answer": "6", "active": false},
+		{"question": "1+6", "answer": "7", "active": false},
+	],
+};

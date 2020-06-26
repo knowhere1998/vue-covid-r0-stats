3cd8fb8547c548677125251f387e34738f37e187
knowhere1998 <darekar.amey@gmail.com>
2020-06-26 20:47:16 +0530

    Fixed indentation in src/main.js

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import AxiosPlugin from 'vue-axios-cors';
 import moment from 'moment';
 import { ToggleButton } from 'vue-js-toggle-button';
 
+Vue.component('faq-modal', require('./components/FaqModal.vue').default);
 Vue.component('pulse-loader', require('vue-spinner/src/PulseLoader.vue'));
 Vue.component('ToggleButton', ToggleButton)
 Vue.component('apexchart', VueApexCharts)
@@ -17,5 +18,5 @@ Vue.config.productionTip = false
 Vue.use(AxiosPlugin);
 
 new Vue({
-  render: h => h(App),
+	render: h => h(App)
 }).$mount('#app')

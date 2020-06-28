3f3f82b576d6073ca8d656c70030c8f08618cb72
knowhere1998 <darekar.amey@gmail.com>
2020-06-28 18:40:26 +0530

    Updated Links in Sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,8 +5,9 @@
 				<p class="text-xl font-semibold pl-2 pb-5">About this site</p>
 				<p class="pb-2">As of today, these stats do not represent exact values for reproduction number according to R<sub>t</sub>, but provide an insight to progression of the virus in India till now, from the beginning of the pandemic.</p>
 				<a class="link cursor-pointer" @click="launchModal()">FAQ</a>
-				<a class="link" href="https://github.com/knowhere1998/">GitHub</a>
-				<a class="link" href="https://twitter.com/Kn0wh3re">twitter</a>
+				<a class="link" :href="getAPIURL()">Data API</a>
+				<a class="link" href="https://github.com/knowhere1998/vue-covid-rt-stats/">GitHub Repository</a>
+				<a class="link" href="https://twitter.com/Kn0wh3re">Twitter</a>
 				<a class="link" href="https://linkedin.com/in/amey-darekar-47950b173/">LinkedIn</a>				
 			</div>
 		</div>
@@ -14,6 +15,7 @@
 </template>
 
 <script>
+import CONSTANTS from './../constants.js';
 
 export default  {
 	name: 'Sidebar',
@@ -23,6 +25,9 @@ export default  {
 	methods: {
 		launchModal() {
 			this.$emit('update:modalStatus', true);
+		},
+		getAPIURL() {
+			return CONSTANTS.API_URL;
 		}
 	}
 };

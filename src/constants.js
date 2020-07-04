340424acf95870a5112fcd8685c45db3d0ba31ea
knowhere1998 <darekar.amey@gmail.com>
2020-07-04 18:17:22 +0530

    Updated faqs

--- src/constants.js
+++ src/constants.js
@@ -3,32 +3,49 @@ export default {
 	'questions' : [
 		{
 			"question": "What is R<sub>t</sub>/R<sub>0</sub>?",
-			"answer": " \
-				R<sub>0</sub>, pronounced “R naught,” is a mathematical term that indicates how contagious an infectious disease is. It’s also referred to as the reproduction number. As an infection is transmitted to new people, it reproduces itself."
+			"answer":  "R<sub>0</sub>, pronounced “R naught,” is a mathematical term that indicates how contagious an infectious disease is. It’s also referred to as the reproduction number. As an infection is transmitted to new people, it reproduces itself."
 		},
 		{
 			"question": "Why R<sub>0</sub>?",
-			"answer": "Answer"
+			"answer":  "<p>R<sub>0</sub> tells you the average number of people who will contract a contagious disease from one person with that disease. For example, if a disease has an R<sub>0</sub> of 4, a person who has the disease will transmit it to an average of 4 other people. That replication will continue if no one has been vaccinated against the disease or is already immune to it in their community. <a style='color: red; text-decoration: underline red;' href='https://en.wikipedia.org/wiki/Basic_reproduction_number'>Know More</a></p>"
 		},
 		{
 			"question": "What does the 'R<sub>t</sub>' value mean?",
-			"answer": "Answer"
+			"answer":  "<p>Three possibilities exist for the potential transmission of a disease, depending on the R<sub>t</sub> value:</p>\
+				<ul class='list-disc pl-5'> \
+					<li> \
+					</p>If Rt is less than 1, each existing infection spreads to less than one new infection. This means the desease spread will decline and it will eventually die out.</p> \
+					</li> \
+					<li> \
+					</p>If Rt is 1, each existing infection causes one new infection. This means the \
+					desease spread will stay stable but there won't be an outbreak.</p> \
+					</li> \
+					<li> \
+					</p>If Rt is greater than 1, each existing infection spreads to more than one new infection. This means the \
+					desease will spread among the community and there may be an outbreak or even a pandemic.</p> \
+					</li> \
+				</ul> \
+				<p>The Rt is only applicable to the population that is completely vulnerable to the disease.</p>"
 		},
 		{
-			"question": "How is it calculated",
-			"answer": "Answer"
+			"question": "How is it calculated?",
+			"answer":  "<p>answer</p>"
 		},
 		{
 			"question": "why 15 days?",
-			"answer": "Answer"
+			"answer":  "<p>Answer</p>"
 		},
 		{
 			"question": "why are you developing this and why now?",
-			"answer": "Answer"
+			"answer":  "<p>Answer</p>"
 		},
 		{
-			"question": "Test question",
-			"answer": "Answer"
+			"question": "Where do you get your data from?",
+			"answer":  "<p>Answer</p>"
+		},
+		{
+			"question": "Can I use this data and if yes, how do I access it?",
+			"answer":  "<p>Answer</p>"
 		},
 	],
 };

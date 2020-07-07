00089903109610d6dcde5ef325fe7e8c966adf4e
knowhere1998 <darekar.amey@gmail.com>
2020-07-07 23:18:20 +0530

    Updated sidebar to include the dev contact card

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,13 +2,23 @@
 	<aside>
 		<div class="w-full lg:flex lg:flex-col lg:sticky lg:top-0">
 			<div class="border bg-white lg:flex lg:flex-col shadow-xl mt-4 p-6">
-				<p class="text-xl font-semibold pl-2 pb-5">About this site</p>
-				<p class="pb-2">These stats do not represent exact values for reproduction number according to R<sub>t</sub> and keep changing daily as the model is implemented on time partitioned data using basic reproduction number method. It provides an insight to progression of the virus in India as of today, starting from the beginning of the pandemic.</p>
-				<a class="link cursor-pointer" @click="launchModal()">FAQ</a>
-				<a class="link" :href="getAPIURL()">Data API</a>
-				<a class="link" href="https://github.com/knowhere1998/vue-covid-rt-stats/">GitHub Repository</a>
-				<a class="link" href="https://twitter.com/a3darekar">Twitter</a>
-				<a class="link" href="https://linkedin.com/in/amey-darekar-47950b173/">LinkedIn</a>				
+				<p class="text-xl font-semibold pl-2 mb-5">About this site</p>
+				<p class="mb-2">These stats do not represent exact values for reproduction number according to R<sub>t</sub> and keep changing daily as the model is implemented on time partitioned data using basic reproduction number method. It provides an insight to progression of the virus in India as of today, starting from the beginning of the pandemic.</p>
+				<a class="link link-underlined cursor-pointer" @click="launchModal()">FAQs</a>
+				<a class="link link-underlined" :href="getAPIURL()">Recompiled data API</a>
+				<a class="link link-underlined" href="https://github.com/knowhere1998/vue-covid-rt-stats/">GitHub Repository</a>
+			</div>
+			<div class="border bg-white lg:flex lg:flex-col shadow-xl mt-4 p-6">
+				<p class="text-xl font-semibold pl-2 mx-auto mb-5">Site made by
+					<a class="link" href="https://about.me/a3darekar">a3darekar</a>
+				</p>
+				<p class="mb-6 text-lg mx-auto">Get in touch!</p>
+				<div class="links flex justify-center px-4">
+					<a class="link link-underlined mx-8" href="mailto:darekar.amey@gmail.com"><img src="https://img.icons8.com/material-rounded/24/000000/important-mail.png"/></a>
+					<a class="link link-underlined mx-8" href="https://twitter.com/a3darekar"><img src="https://img.icons8.com/android/24/000000/twitter.png"/></a>
+					<a class="link link-underlined mx-8" href="https://github.com/knowhere1998/"><img src="https://img.icons8.com/material-outlined/24/000000/github.png"/></a>
+					<a class="link link-underlined mx-8" href="https://linkedin.com/in/amey-darekar-47950b173/"><img src="https://img.icons8.com/android/24/000000/linkedin.png"/></a>
+				</div>
 			</div>
 		</div>
 	</aside>
@@ -36,6 +46,9 @@ export default  {
 <style scoped>
 	.link {
 		color: red;
+	}
+
+	.link-underlined {
 		text-decoration: underline red;
 	}
 </style>
